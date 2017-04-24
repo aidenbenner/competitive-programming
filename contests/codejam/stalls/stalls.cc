cd1cda81aaf6c62306b1ced7d195bbd53e0f2844
aidenbenner <aiden.benner@gmail.com>
2017-04-24 18:06:20 -0400

    update

---
+++ contests/codejam/stalls/stalls.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string> 
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+
+#define INF 1e9
+#define EPS 1e-9
+#define pb push_back
+
+using namespace std;
+
+typedef vector<vector<pair<int,int>>> Adjlist;
+typedef unsigned long long ull;
+typedef long long ll;
+
+
+
+
+int main()
+{
+  int CASES; 
+  cin >> CASES;
+  for(int t = 1; t<=CASES; t++)
+  {
+    ull in;
+    ull k;
+    cin >> k >> in;
+
+
+    int mid = in / 2; 
+
+    while()
+
+
+
+
+    printf("Case #%d: %lld %lld\n", t, out,outh);
+  }
+}
